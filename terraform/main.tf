ae3b70d7c5c7964a9a256148ad3b4a3b315ce71f
kristine-lin <kristine_lin@icloud.com>
2024-09-17 21:33:27 +0800

    deploy to dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ module "ecs" {
       memory = 1024
       # Container definition(s)
       container_definitions = {
-        kristine-ecs-container = { #container name -> Change
+        kristine-ecs-container = { #container name -> Changed
           essential = true
           image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
           port_mappings = [
